c161751d27e16effc0602b65a4167cbde2d35686
the furious dev <so@tfwno.gf>
2021-05-08 09:36:21 +0800

    fix: timings

--- src/libs/timings.ts
+++ src/libs/timings.ts
@@ -1,2 +1,5 @@
-export const timeIn = 'cron(0 23 ? * MON-FRI *)';
+// gmt+8 equivalent is cron(0 7 ? * MON-FRI *)
+export const timeIn = 'cron(0 23 ? * SUN-THU *)';
+
+// gmt+8 equivalent is cron(0 5 ? * MON-FRI *)
 export const timeOut = 'cron(0 9 ? * MON-FRI *)';
