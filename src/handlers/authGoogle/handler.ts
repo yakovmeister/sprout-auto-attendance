59365fcc01c2b648d464d944f22da2a97adfce36
the furious dev <so@tfwno.gf>
2021-05-27 11:49:59 +0800

    feature: add web client support checking

--- src/handlers/authGoogle/handler.ts
+++ src/handlers/authGoogle/handler.ts
@@ -4,18 +4,24 @@ import allowPolicy from '@handlers/authGoogle/allowPolicy';
 import denyPolicy from '@handlers/authGoogle/denyPolicy';
 
 const main = async (event: APIGatewayTokenAuthorizerEvent) => {
+  const { ALLOWED_USER, GOOGLE_CLIENT_ID, WEB_CLIENT } = process.env;
+  
+  if (!WEB_CLIENT || ['no'].includes(WEB_CLIENT)) {
+    return denyPolicy();
+  }
+
   const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
   const token = event.authorizationToken;
 
   try {
     const ticket = await client.verifyIdToken({
       idToken: token,
-      audience: process.env.GOOGLE_CLIENT_ID
+      audience: GOOGLE_CLIENT_ID
     });
 
     const { email, name, picture } = ticket.getPayload();
     
-    if (email !== process.env.ALLOWED_USER) {
+    if (email !== ALLOWED_USER) {
       return denyPolicy();
     }
     
